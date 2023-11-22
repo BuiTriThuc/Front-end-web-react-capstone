403d4c1c9f6b17ea753accc4562a9e89eb86d824
ptKien <phamthanhkien2k1@gmail.com>
2023-11-22 15:26:49 +0700

    update and to top

--- app/staff/listPropertyType/ListPropertyType.tsx
+++ app/staff/listPropertyType/ListPropertyType.tsx
@@ -8,6 +8,7 @@ import axios from 'axios';
 import GetPropertyViewStaff from '@/app/actions/getPropertyViewStaff';
 import GetPropertyTypeStaff from '@/app/actions/getPropertyTypeStaff';
 import { PropertyType } from '@/app/components/map/type';
+import ModalEditPropertyType from './ModalEditPropertyType';
 interface IPropertyType {
   id: number;
   propertyTypeName: string;
@@ -45,13 +46,17 @@ const ListPropertyType: React.FC<ListPropertyTypeProps> = () => {
     sortBy: 'id',
   });
   const [searchName, setSeachName] = useState<string>('');
-  const fetchPropertyType = async () => {
+  const fetchPropertyType = async (id?: number) => {
     const responsePropertyType = await GetPropertyTypeStaff({
       searchName: searchName,
       pageable: pageable,
     });
-    console.log('content', responsePropertyType);
-    setPropertyViewList(responsePropertyType.content);
+    {
+      const result = responsePropertyType.content.filter((element: any) => element.id != id);
+      const theFilterOut = responsePropertyType.content.filter((element: any) => element.id == id);
+      result.splice(0, 0, ...theFilterOut);
+      setPropertyViewList(result);
+    }
     setTotalPages(responsePropertyType.totalPages);
   };
   useEffect(() => {
@@ -70,7 +75,7 @@ const ListPropertyType: React.FC<ListPropertyTypeProps> = () => {
       <div className="py-10">
         <Table>
           <Table.Head>
-            <Table.HeadCell>ID</Table.HeadCell>
+            <Table.HeadCell>No</Table.HeadCell>
             <Table.HeadCell>Property View Name</Table.HeadCell>
             <Table.HeadCell>Property View Description</Table.HeadCell>
             <Table.HeadCell>
@@ -81,17 +86,22 @@ const ListPropertyType: React.FC<ListPropertyTypeProps> = () => {
             {propertyViewList.map((item: IPropertyType, index: any) => (
               <Table.Row key={item.id} className="bg-white dark:border-gray-700 dark:bg-gray-800">
                 <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                  {item.id}
+                  {pageable.pageNo * pageable.pageSize + index + 1}
                 </Table.Cell>
                 <Table.Cell>{item.propertyTypeName}</Table.Cell>
                 <Table.Cell>{item.propertyTypeDescription}</Table.Cell>
                 <Table.Cell>
-                  <a
+                  {/* <a
                     href="#"
                     className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
                   >
                     Edit
-                  </a>
+                  </a> */}
+                  <ModalEditPropertyType
+                    id={item.id}
+                    propertyTypeItem={item}
+                    callback={() => fetchPropertyType(item.id)}
+                  />
                 </Table.Cell>
               </Table.Row>
             ))}
